3c123ada00651c7be506b33d3d78cc221ea3425c
GamerPP <callumtomkins260505@gmail.com>
2021-11-08 16:41:33 +0000

    update

--- mem.hpp
+++ mem.hpp
@@ -6,6 +6,5 @@
 namespace Mem {
 	void Patch(BYTE* dst, BYTE* src, unsigned int size);
 	void Nop(BYTE* dst, unsigned int size);
-	uintptr_t findSig(const char* sig);
 	uintptr_t ResolveMultiLvlPtr(uintptr_t ptr, std::vector<unsigned int> offsets);
 }
